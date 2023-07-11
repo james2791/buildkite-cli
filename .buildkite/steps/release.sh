24798321292be1dee7b2bf6df34fefacc3eaaa6c
Ben McNicholl <benmcnicholl@gmail.com>
2023-07-11 14:20:37 +1000

    Remove signing. Add release steps

--- .buildkite/steps/release.sh
+++ .buildkite/steps/release.sh
@@ -19,22 +19,22 @@ git show-ref --tags -d
 
 git fetch origin
 
-echo "--- importing GPG Secret Key"
+# echo "--- importing GPG Secret Key"
 
-if [ -z "$GPG_SECRET_KEY_BASE64" ]
-then
-      echo "\$GPG_SECRET_KEY_BASE64 env variable must contain a base64 encoded GPG secret key"
-      exit 1
-fi
+# if [ -z "$GPG_SECRET_KEY_BASE64" ]
+# then
+#       echo "\$GPG_SECRET_KEY_BASE64 env variable must contain a base64 encoded GPG secret key"
+#       exit 1
+# fi
 
-echo "${GPG_SECRET_KEY_BASE64}" | base64 -d | gpg --import --no-tty --batch --yes
+# echo "${GPG_SECRET_KEY_BASE64}" | base64 -d | gpg --import --no-tty --batch --yes
 
-gpg --list-secret-keys
+# gpg --list-secret-keys
 
-# GPG_FINGERPRINT is read by goreleaser
-export GPG_FINGERPRINT="$(gpg --list-secret-keys --with-colons 2> /dev/null | grep '^sec:' | cut --delimiter ':' --fields 5)"
+# # GPG_FINGERPRINT is read by goreleaser
+# export GPG_FINGERPRINT="$(gpg --list-secret-keys --with-colons 2> /dev/null | grep '^sec:' | cut --delimiter ':' --fields 5)"
 
-echo "GPG_FINGERPRINT=${GPG_FINGERPRINT}"
+# echo "GPG_FINGERPRINT=${GPG_FINGERPRINT}"
 
 echo "--- Checking GitHub Token"
 if [ -z "$GITHUB_TOKEN" ]
